1dc4940892c038fab82ca759d0495523239dc746
grugru <y6r8ygh.axm6f@slmail.me>
2025-03-08 11:46:27 +0200

    Added main function

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const connect_db = require("./database");
+function main()
+{
+    const connect_db = require("./database");
 
-pg_client = connect_db("postgres://localhost/satellite_gis");
+    pg_client = connect_db("postgres://localhost/satellite_gis");
+    run(pg_client);
+}
 
-async function run()
+async function run(pg_client)
 {
     try {
-        //const tables = pgis.list_tables({ filter: "table_type = \'BASE TABLE\'" });
-        //console.log("Tables: ", tables);
         const query_res = await pg_client.query("SELECT * FROM tbl_test")
         console.log("We have db: ", query_res);
     } catch (err) {
         console.error("Why you have error :( ", err);
     } finally {
-        //await pgclient.end();
+        await pg_client.end();
     }
 }
 
-run();
+if (require.main === module) {
+    main();
+}

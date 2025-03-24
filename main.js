0f1db7145b288a218be1e88205f60cd9da563f4a
grugru <y6r8ygh.axm6f@slmail.me>
2025-03-24 09:20:19 +0200

    Minor main.js change - does not compile

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ const areas = require("./geography/areas").default;
 
 function main()
 {
-    /*const test_cluj = new areas.RectangularArea(areas.test_areas_unfitted.small_cluj);
-    console.log(test_cluj);*/
+    const test_cluj = new areas.RectangularArea(areas.test_areas_unfitted.small_cluj);
+    console.log(test_cluj);
 
-    server.run();
+    //server.run();
 
-    /*const req = new api.ApiRequest(util.generate_rectangle(test_cluj.area), "2025-01-01", "2025-01-06");
+    const req = new api.ApiRequest(util.generate_rectangle(test_cluj.area), "2025-01-01", "2025-01-06");
 
     req.post(api.ApiRequest.urls.mndwi).then(
         (result) => {
             console.log(JSON.stringify(result, null, 2));
         }
-    );*/
+    );
 }
 
 if (require.main === module) {

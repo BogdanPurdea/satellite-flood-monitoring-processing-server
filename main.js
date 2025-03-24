0ab8d1378ab1f26f21d1041b44be850962319ac6
grugru <y6r8ygh.axm6f@slmail.me>
2025-03-24 08:28:00 +0200

    Integrated server.js into main

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ const connect_db = require("./database/database").default;
 const api = require("./api/api_request").default;
 const util = require("./util/util").default;
 const areas = require("./geography/areas").default;
+const server = require("./server/server").default;
 
 function main()
 {
-    const test_cluj = new areas.RectangularArea(areas.test_areas_unfitted.small_cluj);
-    console.log(test_cluj);
+    /*const test_cluj = new areas.RectangularArea(areas.test_areas_unfitted.small_cluj);
+    console.log(test_cluj);*/
 
-    const req = new api.ApiRequest(util.generate_rectangle(test_cluj.area), "2025-01-01", "2025-01-06");
+    server.run();
+
+    /*const req = new api.ApiRequest(util.generate_rectangle(test_cluj.area), "2025-01-01", "2025-01-06");
 
     req.post(api.ApiRequest.urls.mndwi).then(
         (result) => {
             console.log(JSON.stringify(result, null, 2));
         }
-    );
+    );*/
 }
 
 if (require.main === module) {
